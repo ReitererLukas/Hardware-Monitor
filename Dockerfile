cfd03040797c3b6b0a88b15eb5701b0986abb859
ReitererLukas <reiluc17@htl-kaindorf.at>
2021-10-18 20:11:56 +0200

    Docker containers for server.py

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.8.5
+
+WORKDIR /server
+
+RUN pip3 install psutil websockets
+
+COPY server.py .
+
+CMD ["python3","server.py"]
--- server.py
+++ server.py
@@ -2,21 +2,24 @@ import asyncio
 import websockets
 import json
 import psutil
-from sys import argv
+import os
+from platform import system
+
 
 async def monitor(websocket, path):
     while True:
         await websocket.send(json.dumps({
             "cpu": psutil.cpu_percent(),
             "memory": psutil.virtual_memory().percent,
-            "disk": psutil.disk_usage("C:/").percent,
+            "disk": psutil.disk_usage("C:/" if system() == "Windows" else "/").percent,
             "network": len(psutil.net_connections()),
             "pids": len(psutil.pids()),
         }))
         await asyncio.sleep(0.5)
 
 async def main():
-    async with websockets.serve(monitor, "localhost", argv[1]):
+    port = os.getenv('PORT', '6789')
+    async with websockets.serve(monitor, "0.0.0.0", port):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
